42f605e406018fb2e7ddb0c3aa7ace38c4f6c70d
Xecades <i@xecades.xyz>
2025-02-16 09:58:16 +0800

    fix: timezone error

--- src/utils/stats.ts
+++ src/utils/stats.ts
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime.js";
 import locale_zhcn from "dayjs/locale/zh-cn.js";
+import utc from "dayjs/plugin/utc";
 import wc from "words-count";
 
 import type { Entry } from "../entry";
@@ -8,6 +9,7 @@ import type { Entry } from "../entry";
 // @ts-ignore This is a bug in the words-count package
 const wordsCount = wc.wordsCount as typeof wc;
 dayjs.extend(relativeTime);
+dayjs.extend(utc);
 dayjs.locale(locale_zhcn);
 
 export const wordCount = (entries: Entry[]) =>
@@ -27,10 +29,11 @@ export const recentUpdates = (entries: Entry[]) => {
     const t = (e: Entry) => new Date(e.time.updated).getTime();
     let count = 0;
     const toHtml = (e: Entry) => {
+        const d = dayjs.utc(e.time.updated).utcOffset(8);
         const i = ["a", "b", "c"][count++];
         const icon = `<font-awesome-icon class="icon" icon="fa-solid fa-${i}" />`;
         const fmt = "YYYY-MM-DD HH:mm:ss";
-        const alt = `更新于 ${dayjs(e.time.updated).format(fmt)}`;
+        const alt = `更新于 ${d.format(fmt)} (UTC+8)`;
         return `<p> ${icon} <Anchor href="${e.url}" title="${alt}">${e.front_matter.title}</Anchor></p>`;
     };
 
